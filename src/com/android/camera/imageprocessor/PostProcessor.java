ebaaa515afcf2e766214754fdfea7d75c168fd08
Alexander Martinz <amartinz@shiftphones.com>
2021-04-07 21:00:00 +0200

    PostProcessor: only use native methods if library is loaded
    Also remove unused code.
    
    Change-Id: I67b4d252a493ebfcc293c0c852b12c14be3dada6
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- src/com/android/camera/imageprocessor/PostProcessor.java
+++ src/com/android/camera/imageprocessor/PostProcessor.java
@@ -108,6 +108,8 @@ public class PostProcessor{
     public static final int FILTER_DEEPZOOM = 8;
     public static final int FILTER_MAX = 9;
 
+    private static boolean mIsSupported = false;
+
     //BestPicture requires 10 which is the biggest among filters
     private static final int MAX_REQUIRED_IMAGE_NUM = 11;
     private int mCurrentNumImage = 0;
@@ -699,6 +701,10 @@ public class PostProcessor{
     }
 
     public boolean isSelfieMirrorOn() {
+        if (!mIsSupported) {
+            return false;
+        }
+
         if (SettingsManager.getInstance() != null &&
                 SettingsManager.getInstance().getValue(SettingsManager.KEY_SELFIEMIRROR) != null &&
                 SettingsManager.getInstance().getValue(SettingsManager.KEY_SELFIEMIRROR).equalsIgnoreCase("on")) {
@@ -1258,26 +1264,6 @@ public class PostProcessor{
         return mTotalCaptureResultList.get(0);
     }
 
-    private ImageFilter.ResultImage resizeImage(ImageFilter.ResultImage oldImage, Size newSize) {
-        ImageFilter.ResultImage newImage = new ImageFilter.ResultImage(
-                ByteBuffer.allocateDirect(newSize.getWidth() * newSize.getHeight() * 3/2),
-                new Rect(0, 0,
-                        newSize.getWidth(), newSize.getHeight()),
-                newSize.getWidth(), newSize.getHeight(), newSize.getWidth());
-        int ratio = nativeResizeImage(oldImage.outBuffer.array(), newImage.outBuffer.array(),
-                oldImage.width, oldImage.height, oldImage.stride, newSize.getWidth(), newSize.getHeight());
-        newImage.outRoi = new Rect(oldImage.outRoi.left/ratio, oldImage.outRoi.top/ratio,
-                                       oldImage.outRoi.right/ratio, oldImage.outRoi.bottom/ratio);
-        if(newImage.width < newImage.outRoi.width()) {
-            newImage.outRoi.right = newImage.width;
-        }
-        if(newImage.height < newImage.outRoi.height()) {
-            newImage.outRoi.bottom = newImage.height;
-        }
-        Log.d(TAG, "Image is resized by SW with the ratio: "+ratio+" oldRoi: "+oldImage.outRoi.toString());
-        return newImage;
-    }
-
     ImageReader.OnImageAvailableListener processedImageAvailableListener = new ImageReader.OnImageAvailableListener() {
         @Override
         public void onImageAvailable(ImageReader reader) {
@@ -1347,10 +1333,14 @@ public class PostProcessor{
         }
     }
 
-    private native int nativeNV21Split(byte[] srcYVU, ByteBuffer yBuf, ByteBuffer vuBuf, int width, int height, int srcStride, int dstStride);
-    private native int nativeResizeImage(byte[] oldBuf, byte[] newBuf, int oldWidth, int oldHeight, int oldStride, int newWidth, int newHeight);
     private native int nativeFlipNV21(byte[] buf, int stride, int height, int gap, boolean isVertical);
     static {
-        System.loadLibrary("jni_snapimageutil");
+        try {
+            System.loadLibrary("jni_snapimageutil");
+            mIsSupported = true;
+        } catch (UnsatisfiedLinkError e) {
+            Log.d(TAG, e.toString());
+            mIsSupported = false;
+        }
     }
 }
